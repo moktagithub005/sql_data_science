245663a2f0614711d58c2e8afef1bce13c603ede
moktagithub005 <moktaajay@gmail.om>
2026-01-07 22:42:09 +0530

    add all code files

---
+++ sql_lec1.sql
@@ -0,0 +1,38 @@
+-- DQL 
+-- SELECT and FROM
+USE salesdb;
+
+SELECT 
+firstname,
+country,
+score
+FROM customers;
+
+-- WHERE CLAUSE
+SELECT 
+score
+FROM customers
+WHERE country='india';
+
+-- sort your data
+-- ASC and DESC
+SELECT *
+FROM customers
+ORDER BY score  DESC;
+
+SELECT *
+FROM customers
+ORDER BY score  ASC;
+
+-- NESTED SORTING
+SELECT *
+FROM cutomers
+ORDER BY 
+	country ASC,
+	score DESC;
+-- GROUP BY 
+SELECT 
+country,
+SUM(score) AS  total_score
+FROM customers
+GROUP BY country
